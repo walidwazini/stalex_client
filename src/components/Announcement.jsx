62489872ca4141021d539264a81829a59e5b70ba
Walid Wazini <wazini25@gmail.com>
2022-11-23 17:01:27 +0800

    announcement comp

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import "swiper/css/scrollbar";
+
+const TextOne = () => (
+  <SwiperSlide>
+    <p className='text-center text-sm font-medium'>
+      Love Alpine JS? Check out this new course ‼️
+      <a className='underline' href='/alpinejs'>
+        {" "}
+        Learn More &rarr;{" "}
+      </a>
+    </p>
+  </SwiperSlide>
+);
+
+const TextTwo = () => (
+  <SwiperSlide>
+    <p className='text-center text-sm font-medium'>
+      This is text number 2️⃣
+      <a className='underline' href='/alpinejs'>
+        {" "}
+        Learn More &rarr;{" "}
+      </a>
+    </p>
+  </SwiperSlide>
+);
+
+const TextThree = () => (
+  <SwiperSlide>
+    <p className='text-center text-sm font-medium'>
+      This is the 3rd.
+      <a className='underline' href='/alpinejs'>
+        {" "}
+        Learn More &rarr;{" "}
+      </a>
+    </p>
+  </SwiperSlide>
+);
+
+const Announcement = () => {
+  return (
+    <div className='bg-slate-200 px-4 py-3 text-black'>
+      <Swiper
+        modules={[Navigation, Pagination, Autoplay]}
+        slidesPerView={1}
+        autoplay={{ delay: 3500 }}
+        pagination
+        loop
+        speed={800}
+      >
+        {TextOne()}
+        {TextTwo()}
+        {TextThree()}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Announcement;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
+import Announcement from "../components/Announcement";
+
 const HomePage = () => {
   return (
-    <div className='h-screen bg-slate-700 '>
-      <h1 className='text-3xl font-bold underline text-blue-600 '>HomePage</h1>
-    </div>
+    <>
+      <Announcement />
+      <div className='h-screen bg-slate-700 '>
+        <h1 className='text-3xl font-bold underline text-blue-600 '>
+          HomePage
+        </h1>
+      </div>
+    </>
   );
 };
 
