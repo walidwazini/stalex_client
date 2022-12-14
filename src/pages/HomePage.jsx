19feb6a95182e8785cda40e4ee23483c943fa4d8
Walid Wazini <wazini25@gmail.com>
2022-12-14 16:25:16 +0800

    homepage footer 1st ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Announcement from "../components/Announcement";
 import BannerOne from "../components/Homepage/BannerOne";
 import BannerTwo from "../components/Homepage/BannerTwo";
+import Footer from "../components/Homepage/Footer";
 
 const HomePage = () => {
   return (
@@ -11,6 +12,23 @@ const HomePage = () => {
       <div className='h-screen bg-slate-700 '>
         <BannerOne />
         <BannerTwo />
+
+        <div
+          className={`bg-yellow-400 text-black p-4 
+          flex justify-center items-center gap-8 `}
+        >
+          <h1 className={` text-3xl font-semibold uppercase`}>
+            Become a member and get 15% 0ff
+          </h1>
+          <button
+            onClick={() => {}}
+            className={`py-3 px-4  uppercase font-semibold 
+            bg-black text-white hover:text-slate-500`}
+          >
+            Sign up for free
+          </button>
+        </div>
+        <Footer />
       </div>
     </>
   );
